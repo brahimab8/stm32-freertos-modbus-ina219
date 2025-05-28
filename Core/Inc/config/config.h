dacc8d5dfaf57d4d7e446d6d368dd5f5b441a200
Brahim Abdelkafi <70661077+brahimab8@users.noreply.github.com>
2025-05-28 16:16:16 +0200

    chore: move UART_FRAME_TIMEOUT_MS to config.h

--- Core/Inc/config/config.h
+++ Core/Inc/config/config.h
@@ -1,7 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define BOARD_ID  0x01
-#define SENSOR_DEFAULT_POLL_PERIOD    500
+#define BOARD_ID                        0x01
+#define SENSOR_DEFAULT_POLL_PERIOD      500
+#define UART_FRAME_TIMEOUT_MS           10      // Timeout for receiving a full UART command frame (ms)
 
 #endif // CONFIG_H
