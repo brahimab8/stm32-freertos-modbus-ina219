aced25db4aadf7e84e17205c9f4a3d4edec88b47
Brahim Abdelkafi <70661077+brahimab8@users.noreply.github.com>
2025-06-06 13:21:30 +0200

    chore(config): remove unused SENSOR_DEFAULT_POLL_PERIOD define

--- firmware/stm32-i2c-sensor-hub/Core/Inc/config/config.h
+++ firmware/stm32-i2c-sensor-hub/Core/Inc/config/config.h
@@ -2,7 +2,6 @@
 #define CONFIG_H
 
 #define BOARD_ID                        0x01
-#define SENSOR_DEFAULT_POLL_PERIOD      500
 #define UART_FRAME_TIMEOUT_MS           10      // Timeout for receiving a full UART command frame (ms)
 
 #endif // CONFIG_H
